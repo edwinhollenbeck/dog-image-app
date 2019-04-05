3d0243261c334316878c26b46bba097d4360171c
Edwin Hollenbeck <edwin.hollenbeck@yahoo.com>
2019-04-04 21:07:30 -0700

    Updated app to accept a breed.

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 function fetchPictures() {
     $('form').submit(function(event) {
         event.preventDefault();
-        let numDogPics = $('input').val();
+        $('.results-img').remove();
+        let numDogPics = $('#number').val();
+        let dogBreed = $('#breed').val();
         console.log(numDogPics);
-        fetch(`https://dog.ceo/api/breeds/image/random/${numDogPics}`)
+        console.log(dogBreed);
+        fetch(`https://dog.ceo/api/breed/hound-${dogBreed}/images/random/${numDogPics}`)
             .then(response => response.json())
             .then(responseJson => displayPictures(responseJson))
-            .catch(error => alert('Something went wrong'))
+            .catch(error => alert(`Please contact the system administrator.`))
     })
 };
 
 function displayPictures(responseJson) {
+    if (responseJson.status !== "success") {
+        alert(`${responseJson.message}`);
+    } else {
     console.log(responseJson);
     for (i = 0; i < responseJson.message.length; i++) {
-    $('#display-pictures').append(`<img src="${responseJson.message[i]}" class="results-img">`);
+        $('#display-pictures').append(`<img src="${responseJson.message[i]}" class="results-img">`);
+    };
     };
 };
 
